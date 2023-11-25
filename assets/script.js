c17f0d5eb5ecf8a11281c6d25a23146602b47d83
Samantha Allen <samanthaamiraallen@gmail.com>
2023-11-25 11:54:07 -0400

    added quotes on true/false again

--- assets/script.js
+++ assets/script.js
@@ -2,91 +2,91 @@ var questionPackages = [
     {
     question: "What does HTML stand for?",
     options: [
-        { text: "Hyper Text Markup Language", correct: true},
-        { text: "Hotdogs Tacoos Muffins Linguini", correct: false}, 
-        { text: "Home Tv Makeup Living", correct: false}, 
-        { text: "Hyperlinks Text Markup Language", correct: false}
+        { text: "Hyper Text Markup Language", correct: "true"},
+        { text: "Hotdogs Tacoos Muffins Linguini", correct: "false"}, 
+        { text: "Home Tv Makeup Living", correct: "false"}, 
+        { text: "Hyperlinks Text Markup Language", correct: "false"}
     ]
 }, 
 {
     question: "What does CSS stand for?",
     options: [
-        { text:"Cute Style Sheets", correct: false},
-        { text:"Cut Style Shave", correct: false},
-        { text:"Cascading Style Sheets", correct: true},
-        { text:"Cascading Styling Sheet", correct: false}
+        { text:"Cute Style Sheets", correct: "false"},
+        { text:"Cut Style Shave", correct: "false"},
+        { text:"Cascading Style Sheets", correct: "true"},
+        { text:"Cascading Styling Sheet", correct: "false"}
     ]
 },
 {
     question: "What does API stand for?",
     options: [
-        { text:"App Programming Iteration", correct: false},
-        { text:"Apple Programming Interface", correct: false},
-        { text:"Application Program Iteration", correct: false},
-        { text:"Application Programming Interface", correct: true}
+        { text:"App Programming Iteration", correct: "false"},
+        { text:"Apple Programming Interface", correct: "false"},
+        { text:"Application Program Iteration", correct: "false"},
+        { text:"Application Programming Interface", correct: "true"}
     ]
 },
 {
     question: "What HTML element will have the largest heading?",
     options: [
-        { text:"<h6>", correct: false},
-        { text:"<h1>", correct: true},
-        { text:"<h3>", correct: false},
-        { text:"<h4>", correct: false}
+        { text:"<h6>", correct: "false"},
+        { text:"<h1>", correct: "true"},
+        { text:"<h3>", correct: "false"},
+        { text:"<h4>", correct: "false"}
     ]
 },
 {
     question: "What is the default direction for flexbox unless specified?",
     options: [
-        { text:"Row", correct: true},
-        { text:"Column", correct: false},
-        { text:"Flex-start", correct: false},
-        { text:"Flex-end", correct: false}
+        { text:"Row", correct: "true"},
+        { text:"Column", correct: "false"},
+        { text:"Flex-start", correct: "false"},
+        { text:"Flex-end", correct: "false"}
     ]
 },
 {
     question: "What value does a Boolean have?",
     options: [
-        { text:"A string", correct: false},
-        { text:"An Array", correct: false},
-        { text:"A number", correct: false},
-        { text:"True/False", correct: true}
+        { text:"A string", correct: "false"},
+        { text:"An Array", correct: "false"},
+        { text:"A number", correct: "false"},
+        { text:"True/False", correct: "true"}
     ]
 },
 {
     question: "What is the correct HTML element for inserting a line break?",
     options: [
-        { text:"<p>", correct: false},
-        { text:"<br>", correct: true},
-        { text:"<div>", correct: false},
-        { text:"id", correct: false}
+        { text:"<p>", correct: "false"},
+        { text:"<br>", correct: "true"},
+        { text:"<div>", correct: "false"},
+        { text:"id", correct: "false"}
     ]
 },
 {
     question: "Which property is used to set text color?",
     options: [
-        { text:"text-color", correct: false},
-        { text:"background-color", correct: false},
-        { text:"color", correct: true},
-        { text:"textColor", correct: false}
+        { text:"text-color", correct: "false"},
+        { text:"background-color", correct: "false"},
+        { text:"color", correct: "true"},
+        { text:"textColor", correct: "false"}
     ]
 },
 {
     question: "What HTML element do we use to link the external Javascript?",
     options: [
-        { text:"<javascript>", correct: false},
-        { text:"<scripts>", correct: false},
-        { text:"<js>", correct: false},
-        { text:"<script>", correct: true}
+        { text:"<javascript>", correct: "false"},
+        { text:"<scripts>", correct: "false"},
+        { text:"<js>", correct: "false"},
+        { text:"<script>", correct: "true"}
     ]
 },
 {
     question: "How does a for loop start?",
     options: [
-        { text:"for(i = 0; i < 5; i++)", correct: true},
-        { text:"for i = 0; i < 5; i++", correct: false},
-        { text:"for(i = 0; i ++)", correct: false},
-        { text:"for(i =; i < 5)", correct: false}
+        { text:"for(i = 0; i < 5; i++)", correct: "true"},
+        { text:"for i = 0; i < 5; i++", correct: "false"},
+        { text:"for(i = 0; i ++)", correct: "false"},
+        { text:"for(i =; i < 5)", correct: "false"}
     ]
 }];
 
