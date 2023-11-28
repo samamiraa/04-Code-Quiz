97555876bb661f7603bc61265c5bd88f91c82619
Samantha Allen <samanthaamiraallen@gmail.com>
2023-11-27 21:30:25 -0400

    added cooments to new html and js

--- assets/highscore.js
+++ assets/highscore.js
@@ -1,12 +1,19 @@
+// Function to viewHighScore
 function viewHighScore() {
+    // Gets items from localStorage
        var nameInput = localStorage.getItem("fname");
        var getScore = localStorage.getItem("scoreValue");
 
+    //    Creates list element
         var listItems = document.createElement("li");
+        // Adds data from local storage
         listItems.innerHTML = nameInput + " " + getScore + " out of 10!";
 
+        // DOM element
         var orderedList = document.getElementById("highscores");
+        // Appends listItems to orderedList
         orderedList.appendChild(listItems);
     };
 
+    // Calls function to run
     viewHighScore();
